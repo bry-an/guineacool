b782d2da4fc5ea20d5b418f85fe88c39e98541eb
Bryan Yunis <bbyunis@gmail.com>
2024-09-28 16:20:11 -0700

    add some more copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,5 @@
     <h1 class="text-3xl">Welcome to Guineacool.com!</h1>
 
     <p>Wheek wheek we will add a ton of stuff about keeping your guinea pig cool, right hooman...?</p>
+    <p>Where do we even begin wheek wheek...</p>
 </div>
