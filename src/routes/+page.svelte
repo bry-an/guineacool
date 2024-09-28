5ef947a27a3bc53d6d049e82a2370481292cc0eb
Bryan Yunis <bbyunis@gmail.com>
2024-09-28 16:28:47 -0700

    change background color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
-<div style="height:100vh; padding: 0.5rem">
-    <h1 class="text-3xl">Welcome to Guineacool.com!</h1>
+<div style="height:100vh; padding: 0.5rem" class="bg-blue-900">
+    <h1 class="text-3xl text-white">Welcome to Guineacool.com!</h1>
 
-    <div class="mt-4">
+    <div class="mt-4 text-white">
         <p>Wheek wheek we will add a ton of stuff about keeping your guinea pig cool, right hooman...?</p>
         <p>Where do we even begin wheek wheek...</p>
     </div>
