acec460087b7cdaae3b6f4380dde4283efd8e0cc
Bryan Yunis <bbyunis@gmail.com>
2024-09-28 16:31:13 -0700

    added jpg image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <div style="height:100vh; padding: 0.5rem" class="bg-blue-900">
     <h1 class="text-3xl text-white">Welcome to Guineacool.com!</h1>
 
+    <img src="/guinea-pig-vegetables.jpg" class="w-1/3" alt="guinea pig eating vegetables" />
     <div class="mt-4 text-white">
         <p>Wheek wheek we will add a ton of stuff about keeping your guinea pig cool, right hooman...?</p>
         <p>Where do we even begin wheek wheek...</p>
